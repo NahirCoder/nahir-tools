19ef59b233296baf2e02143b22bdbbbf88065ee6
NahirCoder <nahiralam1220@gmail.com>
2026-06-17 18:59:52 +0200

    app.js

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
         console.log("API:", data);
 
+        // SITE
         document.getElementById("name").textContent =
             data.site?.Name || "";
 
@@ -48,6 +49,7 @@ document.addEventListener("DOMContentLoaded", async () => {
                 data.site.Logo[0].url;
         }
 
+        // PORTFOLIO
         const container = document.getElementById("list");
         container.innerHTML = "";
 
@@ -59,29 +61,28 @@ document.addEventListener("DOMContentLoaded", async () => {
             div.className = "project";
 
             div.innerHTML = `
-    <p><strong>Visit the website I made</strong></p>
+                <p><strong>Visit the website I made</strong></p>
 
-    <h3>${f["Website Name"] || ""}</h3>
+                <h3>${f["Website Name"] || ""}</h3>
 
-    <p>
-        <a
-            class="project-link"
-            href="${f["Website URL"] || "#"}"
-            target="_blank"
-        >
-            ${f["Website URL"] || ""}
-        </a>
-    </p>
+                <p>
+                    <a
+                        class="project-link"
+                        href="${f["Website URL"] || "#"}"
+                        target="_blank"
+                    >
+                        ${f["Website URL"] || ""}
+                    </a>
+                </p>
 
-    <p>⭐ ${f.Stars || 0}/5</p>
+                <p>⭐ ${f.Stars || 0}/5</p>
 
-    <p>
-        <strong>Comment by owner:</strong>
-        ${f.Comment || ""}
-    </p>
+                <p>
+                    <strong>Comment by owner:</strong>
+                    ${f.Comment || ""}
+                </p>
 
-    <hr>
-`;
+                <hr>
             `;
 
             container.appendChild(div);
